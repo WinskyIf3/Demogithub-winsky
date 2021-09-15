bf7fec836ae0b9c2b441c4f60b1568138d8b9c98
WinskyIf3 <winsky.belvi@gmail.com>
2021-09-15 09:40:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+let phrase='ma formation javascript';
+ console.log(phrase.indexOf('ma'));
+// console.log(phrase.lastIndexOf('ma'));
+console.log(phrase.indexOf('p'));
+console.log(phrase.charAt(21));
+console.log(phrase.substr(21,1));
+console.log(phrase[21,1]);
+console.log(phrase.replace('javascript','java'));
+console.log(phrase.split(''));
+console.log(phrase.split('').reverse().join(''));
